616d56d572319c138ffb3efd84bd55703321b7c2
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2022-01-06 23:57:10 +0200

    adding folders Arch

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, Resturant, OrderDelivery } from './screens';
+import tabs from './navigation/tabs';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Home'>
+        <Stack.Screen name='Home' component={tabs} />
+        <Stack.Screen name='Resturant' component={Resturant} />
+        <Stack.Screen name='OrderDelivery' component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
